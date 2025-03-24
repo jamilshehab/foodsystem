526e9da32c9722b749ddd6f10885732ed8b5d216
jamilshehab <jamilshehab>
2025-03-24 17:03:17 +0200

    changes added

--- src/Components/Home/ProductSection/HomeProducts/ProductComponent.tsx
+++ src/Components/Home/ProductSection/HomeProducts/ProductComponent.tsx
@@ -5,6 +5,7 @@ import { motion } from "framer-motion";
 import { Link } from "react-router";
 import CustomModal from "../CustomModal";
 import Modal from "react-modal";
+import { FaCartShopping } from "react-icons/fa6";
 const ProductComponent = () => {
   //use state example to display modal
   const [isModal, setShowModal] = useState(undefined);
@@ -66,19 +67,87 @@ const ProductComponent = () => {
           <Modal
             isOpen={isModal === item.id}
             onRequestClose={handleClose}
+            style={{
+              overlay: {
+                position: "fixed",
+                zIndex: 1020,
+                top: 0,
+                left: 0,
+                bottom: 0,
+                width: "100%",
+                height: "100%",
+                background: "rgba(0, 0, 0, 0.5)",
+                display: "flex",
+                alignItems: "center",
+                justifyContent: "center",
+                padding: "1rem", // Prevents edge clipping
+              },
+              content: {
+                background: "white",
+                width: "auto", // Adjusts to content
+                display: "flex",
+                inset: 0,
+                justifyContent: "center",
+                alignItems: "center",
+                maxHeight: "90vh", // Prevents overflowing vertically
+                overflowY: "auto", // Allows scrolling if necessary
+                position: "relative",
+                border: "1px solid #ccc",
+                borderRadius: "0.5rem",
+                // padding: "1.5rem",
+              },
+            }}
             key={item.id}
-            contentLabel="Example Modal"
+            contentLabel="X"
           >
-            <h2>Hello</h2>
-            <button onClick={handleClose}>close</button>
-            <div>I am a modal</div>
-            <form>
-              <input />
-              <button>tab navigation</button>
-              <button>stays</button>
-              <button>inside</button>
-              <button>the modal</button>
-            </form>
+            <button
+              onClick={handleClose}
+              className="absolute top-4 right-4 text-2xl font-bold text-gray-700 hover:text-gray-900 cursor-pointer"
+              aria-label="Close modal"
+            >
+              ✕
+            </button>
+
+            <div className="py-8 bg-white md:py-16 dark:bg-gray-900 antialiased">
+              <div className="max-w-screen-xl px-4 mx-auto 2xl:px-0">
+                <div className="lg:grid lg:grid-cols-2 lg:gap-8 xl:gap-16">
+                  <div className="shrink-0 max-w-md lg:max-w-lg mx-auto">
+                    <img
+                      className="w-full dark:hidden"
+                      src={item.image}
+                      alt=""
+                    />
+                  </div>
+
+                  <div className="mt-6 sm:mt-8 lg:mt-0">
+                    <h1 className="text-xl font-semibold text-gray-900 sm:text-2xl dark:text-white">
+                      {item.title}
+                    </h1>
+                    <div className="mt-4 sm:items-center sm:gap-4 sm:flex">
+                      <p className="text-2xl font-extrabold text-gray-900 sm:text-3xl dark:text-white">
+                        {item.price} $
+                      </p>
+                    </div>
+                    <hr className="my-6 md:my-8 border-gray-200 dark:border-gray-800" />
+
+                    <p className="mb-6 text-gray-500 dark:text-gray-400">
+                      {item.description}
+                    </p>
+                    <div className="mt-6 sm:gap-4 sm:items-center sm:flex sm:mt-8">
+                      <Link
+                        to="#"
+                        title=""
+                        className="text-white mt-4 sm:mt-0 bg-primary hover:bg-primary-400 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-primary-600  focus:outline-none   flex items-center justify-center"
+                        role="button"
+                      >
+                        <FaCartShopping className="me-3" />
+                        Add to cart
+                      </Link>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
           </Modal>
         ))}
       </div>
