97ebefc56d95fa986614e19c7e64d432e5effe72
jamilshehab <jamilshehab>
2025-03-27 13:36:15 +0200

    added a custom breadcrumb

--- src/Components/CustomComponent/Breadcrumb.tsx
+++ src/Components/CustomComponent/Breadcrumb.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 import { MdKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-router";
+import { BreadCrumbProps } from "../../types";
 
-const CheckoutBanner = () => {
+const Breadcrumb: React.FC<BreadCrumbProps> = ({
+  title,
+  firstTitle,
+  secondTitle,
+  thirdTitle,
+  firstLink,
+  secondLink,
+  thirdLink,
+}) => {
   return (
-    <div
+    <section
       className="bg-cover h-[70vh] w-full bg-fixed relative"
       style={{ backgroundImage: "url('/Banner/1.jpg')" }}
     >
@@ -12,7 +21,9 @@ const CheckoutBanner = () => {
       <div className="flex flex-col justify-center items-center h-full space-y-4">
         {/* About Us */}
         <div className="z-10">
-          <h1 className="text-5xl text-white text-center font-bold ">Shop</h1>
+          <h1 className="text-5xl text-white text-center font-bold ">
+            {title}
+          </h1>
         </div>
 
         {/* Breadcrumb Links */}
@@ -20,10 +31,10 @@ const CheckoutBanner = () => {
           <ol className="inline-flex items-center space-x-1 md:space-x-2 rtl:space-x-reverse">
             <li className="inline-flex items-center">
               <Link
-                to="/"
+                to={`/${firstLink}`}
                 className="text-2xl z-10 font-medium text-white hover:text-amber-600"
               >
-                Home
+                {firstTitle}
               </Link>
             </li>
             <li className="flex items-center">
@@ -31,17 +42,30 @@ const CheckoutBanner = () => {
                 <MdKeyboardArrowRight className="text-2xl my-auto text-white z-10" />
               </span>
               <Link
-                to="/checkout"
+                to={`/${secondLink}`}
                 className="my-auto text-white items-center text-2xl z-10 font-medium"
               >
-                Checkout
+                {secondTitle}
               </Link>
             </li>
+            {thirdLink && (
+              <li className="flex items-center">
+                <span>
+                  <MdKeyboardArrowRight className="text-2xl my-auto text-white z-10" />
+                </span>
+                <Link
+                  to={`/${thirdLink}`}
+                  className="my-auto text-white items-center text-2xl z-10 font-medium"
+                >
+                  {thirdTitle}
+                </Link>
+              </li>
+            )}
           </ol>
         </div>
       </div>
-    </div>
+    </section>
   );
 };
 
-export default CheckoutBanner;
+export default Breadcrumb;
---
+++ src/types/index.ts
@@ -0,0 +1,9 @@
+export interface BreadCrumbProps {
+  title:string;
+  firstLink:string;
+  secondLink:string;
+  thirdLink?:string;
+  firstTitle:string;
+  secondTitle:string;
+  thirdTitle?:string;
+}
