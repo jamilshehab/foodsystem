e841c39503876275438b4768bb468f6169cee07c
jamilshehab <jamilshehab>
2025-03-31 11:51:34 +0300

    optimizing some code

--- backend/app.js
+++ backend/app.js
@@ -8,18 +8,19 @@ app.get("/", (req, res) => {
   res.status(200);
   res.send("Welcome to root URL of Server");
 });
+//connected to mongodb
 const connectToMongoDb = async () => {
   try {
     const url =
       "mongodb+srv://user1:pUser%401234!34i5@cluster0.cqaqd2u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
     const client = new MongoClient(url);
-    const connection = await client.connect();
     console.log("connected successfully");
   } catch (error) {
     console.log("connection failed", error);
   }
 };
 connectToMongoDb();
+
 app.listen(PORT, (error) => {
   if (!error) console.log("Server is Successfully Running, " + PORT);
   else console.log("Error occurred, server can't start", error);
