4252ccefc56542b0dced5fc78f7b0a6b2ee3a07d
jamilshehab <jamilshehab>
2025-03-31 10:17:14 +0300

    trying to connect to mongodb

--- backend/app.js
+++ backend/app.js
@@ -1,3 +1,4 @@
+import { MongoClient } from "mongodb";
 const express = require("express");
 
 const app = express();
@@ -12,3 +13,15 @@ app.listen(PORT, (error) => {
   if (!error) console.log("Server is Successfully Running, " + PORT);
   else console.log("Error occurred, server can't start", error);
 });
+
+const connectToMongoDb = async () => {
+  try {
+    const connectionString = process.env.MONGODB_URL || "";
+    const client = new MongoClient(connectionString);
+    const mongodb = await client.connect();
+    console.log("connected successfully", mongodb);
+  } catch (error) {
+    console.log("connection failed", error);
+  }
+};
+connectToMongoDb();
