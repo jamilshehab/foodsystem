ac7b510e663c426a3884f423122c8859c5e37446
jamilshehab <jamilshehab>
2025-03-31 18:53:57 +0300

    mongodb connected successfully

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,7 @@ const connectToMongoDb = async () => {
     const url =
       "mongodb+srv://user1:pUser%401234!34i5@cluster0.cqaqd2u.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
     const client = new MongoClient(url);
+    await client.connect();
     console.log("connected successfully");
   } catch (error) {
     console.log("connection failed", error);
