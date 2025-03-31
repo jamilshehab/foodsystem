abae17fc3638ba4a2ec0139ea8cb98bbe3b30c6a
jamilshehab <jamilshehab>
2025-03-31 20:43:05 +0300

    changes added

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const { connectToMongoDb } = require("./db/db");
+
 const app = express();
 const PORT = 3000;
 
@@ -7,10 +8,10 @@ app.get("/", (req, res) => {
   res.status(200);
   res.send("Welcome to root URL of Server");
 });
-//connected to mongodb
+
 connectToMongoDb();
 
 app.listen(PORT, (error) => {
-  if (!error) console.log("Server is Successfully Running, " + PORT);
-  else console.log("Error occurred, server can't start", error);
+  if (error) console.log("error occured");
+  else console.log("Server is Successfully Running, " + PORT);
 });
